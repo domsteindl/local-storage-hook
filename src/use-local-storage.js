1cbc9d60f8e5c43ef2412108bb79ddff85179676
Dominik Steindl <dominik.steindl@gmx.de>
2026-04-17 19:50:45 +0200

    initial commit + basic loading from localStorage

--- src/use-local-storage.js
+++ src/use-local-storage.js
@@ -1,5 +1,19 @@
-import { useState } from "react";
+import {useState} from "react";
 export default function useLocalStorage(keyName, initialValue) {
-  // Add your solution here
+
+  const [value, setValue]  = useState(() => {
+    try {
+      const currentLocalStorage = JSON.parse( localStorage.getItem(keyName));
+      return currentLocalStorage ?? initialValue;
+    } catch {
+      return initialValue;
+    }
+
+  })
+
+
+
+
+   // localStorage.setItem(keyName, initialValue);
   return useState([]);
 }
